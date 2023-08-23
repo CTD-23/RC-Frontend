4188bdba5edfce8c8e4a89a08ab92c6c59d8dade
sarthakp10 <sarthakphadnis10@gmail.com>
2023-08-23 11:51:25 +0530

    Rc all pages

---
+++ rcfinal/src/Utils/AxiosConfig.js
@@ -0,0 +1,21 @@
+import axios from "axios"; 
+
+export const API_URL = "http://20.198.81.8/"; 
+
+
+export const AxiosInstance = axios.create({
+    // baseURL : "http://20.198.81.8",
+    baseURL : "https://api.ncc.ctd.credenz.in",
+    // withCredentials : true,
+    Headers:{
+        'content-type':'application/json',
+    }
+})
+
+export const addAuthToken = (token) => {
+    if (token) {
+        AxiosInstance.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+    } else {
+        delete AxiosInstance.defaults.headers.common['Authorization'];
+    }
+};
